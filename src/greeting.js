e4c86d1bd4ecf1dd83d377f3bbaa1da1c44d0a78
Volodymyr Melnyk <melnykvova90@gmail.com>
2019-07-01 16:18:54 +0300

    add basic webpack bundler

---
+++ src/greeting.js
@@ -0,0 +1,5 @@
+const greet = (str) => {
+    return `${str} developer`
+}
+
+export { greet };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { greet } from './greeting';
+import './styles/main.scss';
+
+console.log(greet('Hello'));
